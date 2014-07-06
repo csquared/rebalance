3bde841a7a2e48384284b267c915bfe1ec3de2ff
csquared <christopher.continanza@gmail.com>
2014-07-05 17:45:59 -0700

    put it in ze files

--- rebalance.go
+++ rebalance.go
@@ -32,6 +32,7 @@ func getPrices(stocks []string) map[string]float64{
   return prices;
 }
 
+/*
 var targetAllocation = map[string]float64 {
   "SCHP" : 10,
   "SCHC" : 10,
@@ -45,20 +46,53 @@ var currentAllocation = map[string]float64 {
   "SCHB" : 305.7877,
   "SCHV" : 48.2886,
 }
+*/
+
+func parseAllocation(fileName string) (map[string]float64, error) {
+  var allocations = make(map[string]float64)
+
+  allocationData, err := ioutil.ReadFile(fileName);
+  if(err != nil){
+    return nil, err;
+  }
+  err = json.Unmarshal(allocationData, &allocations)
+  if(err != nil){
+    return nil, err;
+  }
+
+  return allocations, nil;
+}
 
 func main() {
+
+  var investLimit int;
+  flag.IntVar(&investLimit, "amount", 1000, "amount to invest")
+  var targetAllocationFile string;
+  flag.StringVar(&targetAllocationFile, "target",
+    "./target-allocation.json", "json file of stock: percent")
+  var currentAllocationFile string;
+  flag.StringVar(&currentAllocationFile, "current",
+    "./current-allocation.json", "json file of stock: number of stocks")
+  flag.Parse()
+
+  targetAllocation, err := parseAllocation(targetAllocationFile);
+  if err != nil {
+    panic(err)
+  }
+  currentAllocation, err := parseAllocation(currentAllocationFile);
+  if err != nil {
+    panic(err)
+  }
+
   stocks := make([]string, 0, len(targetAllocation))
   for symbol := range(targetAllocation){
     stocks = append(stocks, symbol)
   }
+
   prices := getPrices(stocks)
   fmt.Println(prices);
   buys   := make(map[string]int)
   amountInvested := 0.0
-
-  var investLimit int;
-  flag.IntVar(&investLimit, "amount", 1000, "amount to invest")
-  flag.Parse()
 
   for {
     for symbol, allocation := range(currentAllocation) {
